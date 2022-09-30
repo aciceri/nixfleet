cc2308b9bed225cccb17316cbdecb20c6a349f3d
Andrea Ciceri <andrea.ciceri@autistici.org>
2022-09-30 22:25:12 +0200

    Set personal Cachix substituter

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -15,10 +15,12 @@
       trusted-public-keys = [
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "mlabs.cachix.org-1:gStKdEqNKcrlSQw5iMW6wFCj3+b+1ASpBVY2SYuNV2M="
+        "aciceri-fleet.cachix.org-1:e1AodrwmzRWy0eQi3lUY71M41fp9Sq+UpuKKv705xsI="
       ];
       substituters = [
         "https://cache.iog.io"
-        "https://mlabs.cachix.org/"
+        "https://mlabs.cachix.org"
+        "httos://aciceri-fleet.cachix.org"
       ];
     };
 
