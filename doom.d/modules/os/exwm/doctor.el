3a219ee9549e69e5097b4a0e0bae3a0750a65144
Andrea Ciceri <andrea.ciceri@autistici.org>
2022-09-30 17:29:33 +0200

    [WIP] Remove EXWM layer from Doom Emacs

--- doom.d/modules/os/exwm/doctor.el
+++
@@ -1,6 +0,0 @@
-;;; -*- lexical-binding: t; no-byte-compile: t; -*-
-;;; os/exwm/doctor.el
-
-(when (featurep! +firefox)
-  (unless (executable-find "firefox")
-    (warn! "Couldn't find firefox. exwm-firefox will not work")))
