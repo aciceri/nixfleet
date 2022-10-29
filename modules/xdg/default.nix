e71fee8852759828e80c9e1943e358a912bef11a
Andrea Ciceri <andrea.ciceri@autistici.org>
2022-10-29 12:14:20 +0200

    Forgot XDG module

---
+++ modules/xdg/default.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}: {
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+      ];
+      gtkUsePortal = true;
+    };
+  };
+  services.pipewire.enable = true;
+}
