dfe800efc57b217091db9b67fa6576add300f6f7
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-01-20 10:47:58 +0100

    Added `delta` for `git`/`diff`

--- hmModules/git/default.nix
+++ hmModules/git/default.nix
@@ -4,6 +4,8 @@
     email = "andrea.ciceri@autistici.org";
   };
 in {
+
+  home.packages = [pkgs.gitoxide];
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
@@ -29,6 +31,15 @@ in {
         else {
           "ssh://git@github.com/" = {insteadOf = https://github.com/;};
         };
+    };
+    delta = {
+      enable = true;
+      options = {
+        features = "decorations";
+        delta = {
+          line-numbers = true;
+        };
+      };
     };
   };
 }
