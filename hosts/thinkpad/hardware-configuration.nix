bc479b722589fcedd229ae5f7798204c6a368de7
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-04-01 11:40:31 +0200

    `uinput` kernel module for `thinkpad`

--- hosts/thinkpad/hardware-configuration.nix
+++ hosts/thinkpad/hardware-configuration.nix
@@ -14,7 +14,7 @@
 
   boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = [];
+  boot.kernelModules = ["uinput"];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
