a02d4bf5bbd585a5cdddc6a059c0c45e35e20e5f
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-04-01 11:40:09 +0200

    Secrets rekeyed for `mothership`

--- secrets/default.nix
+++ secrets/default.nix
@@ -2,10 +2,11 @@ let
   users.ccr = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5cEUx25pnZiH3eBrE2xNbJ92gJiKSznDUNRzcEL4ti6FlJm+75p4q0hgdqHwStR8+uCWBL6viVFCGutOVMFE5MX1Oc3A8fJdR6H9Rrwvk/1UQzqzc9tWxw1qPLKz+fnPDomjOvNofghCWQRwX3Xf1HnIqvRwELpNbR9i+/cHkDGzLJxkstbt4gol8ywMPkw02QdKk8s5MEd1vawxc+7Chs0JPW57RDqDYFErYys52JLeAViCBB9bofF+KT42LuRXKSjWlvCV9kR5TL49vUeBgzMQWMh++WQdN4m9lpqFqYyc75I49/E0HGf8LChDSS+hvRnb5MbtnVGjEA4WDHyldmJCvUNob5CUo4FjoSPRi+S/J3Ads8D4JVwaJOJEVqmMKEhiQ0Hzk4hwe3eV/VumlZj4U/QjaCrqqi4TW/iP0gNRfzcfiM+G/z5R7w1NMUpTX7oilyKjMQmGnXB857D3SSptS7dwh5OiKhVmrQMRCduooUsj236abqLU28K//RnxhOgh8kDGgoUHApnTiMZNKhgLiR42lKrubNcW1tAAqoNyFLMwwXeMLjh0iP1b5y8ntfNPNIcGb7vcwpS24z/aIjW7rQ4J7x5EBphHGhys6ne+irdhOM8c7kFr+c8+Q2oU0YAtFuMYztAFOHm1e20X00Zvys2nuee+hT9F1NungAQ==";
   hosts = {
     test = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHc46mGCuaKLwNzK/abuedYQLw9h/Cp5MhVb7IHTGh0E root@test";
+    mothership = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFlepPWHE9GvQIBcAQBQPd80oiePSPxGDnMdqpdEqx6I";
   };
 in {
-  "cachix.age".publicKeys = [users.ccr];
+  "cachix.age".publicKeys = [users.ccr hosts.mothership];
   "autistici-password.age".publicKeys = [users.ccr];
-  "magit-forge-github-token.age".publicKeys = [users.ccr];
-  "git-workspace-tokens.age".publicKeys = [users.ccr hosts.test];
+  "magit-forge-github-token.age".publicKeys = [users.ccr hosts.mothership];
+  "git-workspace-tokens.age".publicKeys = [users.ccr hosts.test hosts.mothership];
 }
