1c6e94ad0e852b85df2b4ab3fe2ebfd2e5bba406
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-04-02 16:16:52 +0200

    `fail2ban`

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -1,4 +1,10 @@
 {fleetFlake, ...}: {
-  services.sshd.enable = true;
+  services = {
+    sshd.enable = true;
+    fail2ban = {
+      enable = true;
+      maxretry = 10;
+    };
+  };
   users.users.root.openssh.authorizedKeys.keys = builtins.attrValues (import "${fleetFlake}/lib").keys.users;
 }
