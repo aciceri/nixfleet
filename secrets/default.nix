139c8189206d2a8f58004ffccd32377622ed89df
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-04-11 23:44:02 +0200

    New secrets

--- secrets/default.nix
+++ secrets/default.nix
@@ -6,6 +6,9 @@ in
     "cachix-personal-token.age".publicKeys = [ccr-ssh ccr-gpg mothership thinkpad];
     "magit-forge-github-token.age".publicKeys = [ccr-ssh ccr-gpg mothership thinkpad];
     "git-workspace-tokens.age".publicKeys = [ccr-ssh ccr-gpg mothership thinkpad];
+    "hydra-admin-password.age".publicKeys = [ccr-ssh ccr-gpg mothership];
+    "hydra-github-token.age".publicKeys = [ccr-ssh ccr-gpg mothership];
+    "cache-private-key.age".publicKeys = [ccr-ssh ccr-gpg mothership];
 
     # WireGuard
     "thinkpad-wireguard-private-key.age".publicKeys = [ccr-ssh ccr-gpg thinkpad];
