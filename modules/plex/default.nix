a498187dff54d0104494a9982d1a6d549f261d2b
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-06-19 22:15:13 +0200

    Module for `plex`

---
+++ modules/plex/default.nix
@@ -0,0 +1,13 @@
+{
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+    dataDir = "/mnt/raid/plex";
+  };
+  
+  systemd.tmpfiles.rules = [
+    "d /mnt/raid/plex 770 plex plex"
+  ];
+
+  users.users.plex.extraGroups = [ "transmission" ];
+}
