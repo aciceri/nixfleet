60e9ba7cc02fbcd7a2e85a1ad280a3c45165fc6e
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-10-22 10:21:22 +0200

    Experimenting with `pbp`'s `disko` config

--- hosts/pbp/disko.nix
+++ hosts/pbp/disko.nix
@@ -1,78 +1,80 @@
 _: {
-  disk = {
-    # emmc = {
-    #   device = "/dev/mmcblk2";
-    #   type = "disk";
-    #   content = {
-    #     type = "table";
-    #     format = "gpt";
-    #     partitions = [
-    #       {
-    #         name = "root";
-    #         start = "1MiB";
-    #         end = "-4G";
-    #         part-type = "primary";
-    #         bootable = false;
-    #         content = {
-    #           type = "filesystem";
-    #           format = "ext4";
-    #           mountpoint = "/";
-    #         };
-    #       }
-    #       {
-    #         name = "swap";
-    #         start = "-4G";
-    #         end = "100%";
-    #         part-type = "primary";
-    #         content = {
-    #           type = "swap";
-    #           randomEncryption = true;
-    #         };
-    #       }
-    #     ];
-    #   };
-    # };
-    ssd = {
-      device = "/dev/nvme0n1";
-      type = "disk";
-      content = {
-        type = "table";
-        format = "gpt";
-        partitions = [
-          {
-            name = "ESP";
-            start = "1MiB";
-            end = "1024MiB";
-            bootable = true;
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
-          }
-          {
-            name = "root";
-            start = "1024MiB";
-            end = "-8G";
-            part-type = "primary";
-            bootable = false;
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-            };
-          }
-          {
-            name = "swap";
-            start = "-8G";
-            end = "100%";
-            part-type = "primary";
-            content = {
-              type = "swap";
-              randomEncryption = true;
-            };
-          }
-        ];
+  disko.devices = {
+    disk = {
+      # emmc = {
+      #   device = "/dev/mmcblk2";
+      #   type = "disk";
+      #   content = {
+      #     type = "table";
+      #     format = "gpt";
+      #     partitions = [
+      #       {
+      #         name = "root";
+      #         start = "1MiB";
+      #         end = "-4G";
+      #         part-type = "primary";
+      #         bootable = false;
+      #         content = {
+      #           type = "filesystem";
+      #           format = "ext4";
+      #           mountpoint = "/";
+      #         };
+      #       }
+      #       {
+      #         name = "swap";
+      #         start = "-4G";
+      #         end = "100%";
+      #         part-type = "primary";
+      #         content = {
+      #           type = "swap";
+      #           randomEncryption = true;
+      #         };
+      #       }
+      #     ];
+      #   };
+      # };
+      ssd = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "ESP";
+              start = "1MiB";
+              end = "1024MiB";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            }
+            {
+              name = "root";
+              start = "1024MiB";
+              end = "-8G";
+              part-type = "primary";
+              bootable = false;
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            }
+            {
+              name = "swap";
+              start = "-8G";
+              end = "100%";
+              part-type = "primary";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            }
+          ];
+        };
       };
     };
   };
