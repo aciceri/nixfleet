92bf72e40bce687dccf98f6881830dd4e106f514
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-10-22 10:20:39 +0200

    Fix `nix-index`

--- hmModules/nix-index/default.nix
+++ hmModules/nix-index/default.nix
@@ -1,15 +1,19 @@
 {
   config,
   pkgs,
+  fleetFlake,
+  lib,
   ...
 }: {
+  programs.nix-index.enable = true;
+
   systemd.user.services.nix-index-update = {
     Unit = {Description = "Update nix-index";};
 
     Service = {
       CPUSchedulingPolicy = "idle";
       IOSchedulingClass = "idle";
-      ExecStart = "${pkgs.nix-index}/bin/nix-index --path ${config.programs.password-store.settings.PASSWORD_STORE_DIR}";
+      ExecStart = "${pkgs.nix-index}/bin/nix-index --nixpkgs ${fleetFlake.inputs.nixpkgsUnstable}";
     };
   };
 
