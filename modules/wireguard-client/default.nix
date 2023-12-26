86e08cad69ab96ccbd6c4449b3a5fdcbe356b583
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-12-26 11:01:51 +0100

    WireGuard clients connect only to one `sisko`'s interface

--- modules/wireguard-client/default.nix
+++ modules/wireguard-client/default.nix
@@ -7,18 +7,11 @@
 
   networking.wireguard.interfaces.wg0 = {
     ips = ["${vpn.${config.networking.hostName}.ip}/32"];
-    # TODO having two peers like this a good idea? (they are the same host)
     peers = [
       {
-        publicKey = vpn.sisko.publicKey; # FIXME hardcoding `sisko` here
+        publicKey = vpn.sisko.publicKey;
         allowedIPs = ["10.100.0.0/24"];
         endpoint = "vpn.aciceri.dev:51820";
-        persistentKeepalive = 25;
-      }
-      {
-        publicKey = vpn.sisko.publicKey; # FIXME hardcoding `sisko` here
-        allowedIPs = ["10.100.0.0/24"];
-        endpoint = "10.1.1.2:51820";
         persistentKeepalive = 25;
       }
     ];
