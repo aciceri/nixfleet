e1da21e8fe57741183e6fb930c396ede8031ff39
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-01-10 01:31:58 +0100

    Option renamed

--- modules/printing/default.nix
+++ modules/printing/default.nix
@@ -2,7 +2,7 @@
   services.avahi.enable = true;
   # Important to resolve .local domains of printers, otherwise you get an error
   # like  "Impossible to connect to XXX.local: Name or service not known"
-  services.avahi.nssmdns = true;
+  services.avahi.nssmdns4 = true;
   hardware.sane.enable = true;
 
   services.printing = {
