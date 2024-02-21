087d001124a7ce3b45e9e5c8c2dc3b08acd3f30e
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-02-21 17:53:00 +0100

    New `sisko` partitioning

--- hosts/sisko/disko.nix
+++ hosts/sisko/disko.nix
@@ -1,5 +1,6 @@
 let
-  emmc = "/dev/mmcblk0";
+  emmc = "/dev/disk/by-id/mmc-SLD64G_0xf6be3ba0";
+  ssd = "/dev/disk/by-id/ata-CT240BX300SSD1_1739E1042F3C";
   # hd1 = "/dev/disk/by-id/ata-WDC_WD10EADS-22M2B0_WD-WCAV52709550";
   # hd2 = "/dev/disk/by-id/ata-WDC_WD10EADX-22TDHB0_WD-WCAV5V359530";
   hd = "/dev/disk/by-id/ata-WDC_WD10EADS-22M2B0_WD-WCAV52709550-part1";
@@ -7,114 +8,34 @@ let
 in {
   disko.devices = {
     disk = {
-      emmc = {
+      ssd = {
+        device = ssd;
         type = "disk";
-        device = emmc;
         content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              start = "32.8kB";
-              end = "12.6MB";
-              name = "uboot";
-              bootable = true;
-            }
-            {
-              name = "NIXOS_ROOTFS";
-              start = "13.6MB";
-              end = "100%";
-              flags = ["legacy_boot"];
+          type = "gpt";
+          partitions = {
+            ESP = {
+              label = "ESP";
+              type = "EF00";
+              size = "1G";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              label = "root";
+              size = "100%";
               content = {
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
               };
-            }
-          ];
+            };
+          };
         };
       };
-      # hd = {
-      #   type = "disk";
-      #   device = hd;
-      #   content = {
-      #     type = "table";
-      #     format = "gpt";
-      #     partitions = [
-      #       {
-      #         name = "hd";
-      #         start = "0%";
-      #         end = "100%";
-      #         content = {
-      #           type = "filesystem";
-      #           format = "ext4";
-      #           mountpoint = "/mnt/hd";
-      #         };
-      #       }
-      #     ];
-      #   };
-      # };
-      # hd1 = {
-      #   type = "disk";
-      #   device = hd1;
-      #   content = {
-      #     type = "table";
-      #     format = "gpt";
-      #     partitions = [
-      #       {
-      #         name = "primary";
-      #         start = "0";
-      #         end = "100%";
-      #         content = {
-      #           type = "mdraid";
-      #           name = "raid1";
-      #         };
-      #       }
-      #     ];
-      #   };
-      # };
-      # hd2 = {
-      #   type = "disk";
-      #   device = hd2;
-      #   content = {
-      #     type = "table";
-      #     format = "gpt";
-      #     partitions = [
-      #       {
-      #         name = "primary";
-      #         start = "0";
-      #         end = "100%";
-      #         content = {
-      #           type = "mdraid";
-      #           name = "raid1";
-      #         };
-      #       }
-      #     ];
-      #   };
-      # };
     };
-
-    # mdadm = {
-    #   raid1 = {
-    #     type = "mdadm";
-    #     level = 1;
-    #     content = {
-    #       type = "table";
-    #       format = "gpt";
-    #       partitions = [
-    #         {
-    #           name = "primary";
-    #           start = "0";
-    #           end = "100%";
-    #           content = {
-    #             type = "filesystem";
-    #             format = "ext4";
-    #             mountpoint = "/mnt/raid";
-    #           };
-    #         }
-    #       ];
-    #     };
-    #   };
-    # };
   };
 }
