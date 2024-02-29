98e1c84a06e6096e486c9ccb2fb33c0cb59a75b9
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-02-29 01:54:58 +0100

    New font

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   fonts = {
-    packages = with pkgs; [powerline-fonts dejavu_fonts fira-code fira-code-symbols iosevka iosevka-comfy.comfy emacs-all-the-icons-fonts nerdfonts joypixels etBook];
+    packages = with pkgs; [powerline-fonts dejavu_fonts fira-code fira-code-symbols iosevka iosevka-comfy.comfy emacs-all-the-icons-fonts nerdfonts joypixels etBook vegur];
     fontconfig.defaultFonts = {
       monospace = ["DejaVu Sans Mono for Powerline"];
       sansSerif = ["DejaVu Sans"];
