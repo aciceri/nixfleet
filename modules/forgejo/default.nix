58f8584c3e99cf3a8615633d0fa36999e07832c5
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-03-14 19:10:09 +0100

    Enable emails for `forgejo`

--- modules/forgejo/default.nix
+++ modules/forgejo/default.nix
@@ -11,16 +11,26 @@
         RUN_MODE = "prod"; # set to prod for better logs (worse performance)
         APP_NAME = "git.aciceri.dev";
       };
+      service.ENABLE_NOTIFY_MAIL = true;
       session.COOKIE_SECURE = true;
       service.DISABLE_REGISTRATION = true;
       server = {
         HTTP_PORT = 3002;
         ROOT_URL = "https://git.aciceri.dev";
       };
+      mailer = {
+        ENABLED = true;
+        PROTOCOL = "smtp+starttls";
+        SMTP_ADDR = "smtp.autistici.org";
+        SMTP_PORT = 587;
+        FROM = "andrea.ciceri@autistici.org";
+        USER = "andrea.ciceri@autistici.org";
+      };
       other = {
         SHOW_FOOTER_VERSION = false;
       };
     };
+    mailerPasswordFile = config.age.secrets.autistici-password.path;
     dump.enable = true;
   };
 
