458c00e4997d379092e11c33de83b1e7c86e65f0
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-04-02 12:42:53 +0200

    `btop` settings

--- hmModules/btop/default.nix
+++ hmModules/btop/default.nix
@@ -1,6 +1,10 @@
-{
+{config, ...}: {
   programs.btop = {
     enable = true;
-    settings.update_ms = 100;
+    settings = {
+      update_ms = 100;
+      theme_background = false;
+      color_theme = "${config.programs.btop.package}/share/btop/themes/dracula.theme";
+    };
   };
 }
