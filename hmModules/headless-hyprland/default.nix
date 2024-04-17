25ec5ef86d231f073066f75d3c0470a219b59e47
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-04-17 13:44:56 +0200

    [WIP] New `headless-hyprlan` module

---
+++ hmModules/headless-hyprland/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  originalConfig = config.wayland.windowManager.hyprland.extraConfig;
+  config = builtins.replaceStrings ["SUPER"] [""] originalConfig;
+in {
+  systemd.user.services.headless-hyprland = {
+    Unit.Description = "Headless Hyprland";
+    Service = {
+      Type = "oneshot";
+      ExecStart = ''
+        ${lib.getExe config.wayland.windowManager.hyprland.package} --config ${config}
+      '';
+    };
+  };
+}
