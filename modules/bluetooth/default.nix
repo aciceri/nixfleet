fc65aa5403a8ed317881a2539503a247df548370
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-04-17 13:45:38 +0200

    Bluetooth fix
    Don't know exactly what happened but this fixed it

--- modules/bluetooth/default.nix
+++ modules/bluetooth/default.nix
@@ -1,7 +1,24 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   services.blueman.enable = true;
   hardware.pulseaudio.enable = true;
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        Name = config.networking.hostName;
+        ControllerMode = "dual";
+        FastConnectable = "true";
+        Experimental = "true";
+      };
+      Policy = {
+        AutoEnable = "true";
+      };
+    };
+  };
   services.dbus.packages = with pkgs; [blueman];
   ccr.extraGroups = ["bluetooth"];
 }
