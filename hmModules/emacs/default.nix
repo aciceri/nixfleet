7abf093ddd885493f0965da08ffffab0f8f4de73
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-05-01 10:51:14 +0200

    `llm-workflow-engine` not used anymore

--- hmModules/emacs/default.nix
+++ hmModules/emacs/default.nix
@@ -7,5 +7,4 @@
   ccrEmacs.enable = true;
   home.sessionVariables.EDITOR = lib.mkForce "emacsclient";
   systemd.user.services.emacs.Service.EnvironmentFile = age.secrets.chatgpt-token.path;
-  home.packages = lib.lists.optional pkgs.stdenv.isx86_64 pkgs.llm-workflow-engine;
 }
