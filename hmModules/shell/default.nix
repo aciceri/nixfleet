c17fb32a289e8a38096cebe8894d613756a3edaa
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-09-25 14:20:30 +0200

    Enable `ranger`

--- hmModules/shell/default.nix
+++ hmModules/shell/default.nix
@@ -8,6 +8,13 @@
 {
   programs.bat.enable = true;
 
+  programs.ranger = {
+    enable = true;
+    settings = {
+      "preview_images" = true;
+      "preview_images_method" = "sixel";
+    };
+  };
   programs.direnv = {
     enable = true;
     config = {
