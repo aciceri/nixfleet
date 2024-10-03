5658f13955e13117d920c848636c16b7c7b10a72
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-10-03 21:53:40 +0200

    Remove test settings

--- hmModules/catppuccin/default.nix
+++ hmModules/catppuccin/default.nix
@@ -10,30 +10,4 @@
     platformTheme.name = lib.mkForce "kvantum";
     style.name = lib.mkForce "kvantum";
   };
-
-  # TODO move away
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      font = {
-        normal = {
-          family = "Fira Code";
-          style = "Regular";
-        };
-        bold = {
-          family = "Fira Code";
-          style = "Bold";
-        };
-        italic = {
-          family = "Cascadia Code";
-          style = "Italic";
-        };
-        bold_italic = {
-          family = "Fira Code";
-          style = "Bold Italic";
-        };
-        size = 13;
-      };
-    };
-  };
 }
