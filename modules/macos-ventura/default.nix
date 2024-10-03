1d4f9fecdc26b9c272be2ee42b969d4ffa146323
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-10-03 21:53:05 +0200

    Improve virtualized macos-ventura settings
    Allow ssh into it on the standard port

--- modules/macos-ventura/default.nix
+++ modules/macos-ventura/default.nix
@@ -1,15 +1,24 @@
-{ fleetFlake, ... }:
+{ lib, ... }:
 {
   services.macos-ventura = {
     enable = true;
     cores = 8;
     threads = 8;
-    mem = "8G";
+    mem = "16G";
     vncListenAddr = "0.0.0.0";
+    sshListenAddr = "127.0.0.1";
     extraQemuFlags = [ "-nographic" ];
-    sshPort = 2021;
-    installNix = true;
-    stateless = true;
-    darwinConfig = fleetFlake.darwinConfigurations.archer;
+    sshPort = 2022;
+    vncDisplayNumber = 1; # means port 59001
+    stateless = false;
+    openFirewall = true;
+    autoStart = false;
   };
+
+  programs.ssh.extraConfig = lib.mkAfter ''
+    Host macos-ventura
+    Hostname localhost
+    Port 2022
+    Compression yes
+  '';
 }
