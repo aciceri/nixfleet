c35c00bf75e188bcbbd52070008ef004379537d5
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-10-10 17:06:42 +0200

    Still doesn't work

--- modules/mount-rock5b/default.nix
+++ modules/mount-rock5b/default.nix
@@ -15,7 +15,7 @@
         '';
       in
       [
-        "credentials=${credentials},x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"
+        "credentials=${credentials},x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=1000,gid=1000"
       ];
   };
 }
