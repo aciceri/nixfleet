795b5a05cedb090ff15648bc67953126e57804cb
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-11-08 10:17:57 +0100

    `nixfmt-rf-style` is now called just `nixfmt`

--- checks/default.nix
+++ checks/default.nix
@@ -1,42 +1,27 @@
-{
-  inputs,
-  self,
-  lib,
-  ...
-}:
-{
-  imports = [
-    inputs.treefmt-nix.flakeModule
-    inputs.git-hooks-nix.flakeModule
-  ];
+{ inputs, self, lib, ... }: {
+  imports = [ inputs.treefmt-nix.flakeModule inputs.git-hooks-nix.flakeModule ];
 
-  perSystem =
-    { ... }:
-    {
-      treefmt.config = {
-        projectRootFile = ".git/config";
-        programs = {
-          nixfmt-rfc-style.enable = true;
-          deadnix.enable = false;
-        };
-      };
-
-      pre-commit.settings.hooks = {
-        nixfmt-rfc-style.enable = true;
+  perSystem = { ... }: {
+    treefmt.config = {
+      projectRootFile = ".git/config";
+      programs = {
+        nixfmt.enable = true;
         deadnix.enable = false;
       };
     };
 
-  flake.checks =
-    let
-      build = _: nc: nc.config.system.build.toplevel;
-    in
-    {
-      x86_64-linux = lib.mapAttrs build {
-        inherit (self.nixosConfigurations) picard;
-      };
-      aarch64-linux = lib.mapAttrs build {
-        inherit (self.nixosConfigurations) sisko; # pbp;
-      };
+    pre-commit.settings.hooks = {
+      nixfmt.enable = true;
+      deadnix.enable = false;
     };
+  };
+
+  flake.checks = let build = _: nc: nc.config.system.build.toplevel;
+  in {
+    x86_64-linux =
+      lib.mapAttrs build { inherit (self.nixosConfigurations) picard; };
+    aarch64-linux = lib.mapAttrs build {
+      inherit (self.nixosConfigurations) sisko; # pbp;
+    };
+  };
 }
