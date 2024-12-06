26187a521b0e9912f26d7afd5203d05e0c1339ce
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-12-06 11:41:56 +0100

    `nerdfonts` became `nerd-fonts`

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -1,19 +1,21 @@
 { pkgs, ... }:
 {
   fonts = {
-    packages = with pkgs; [
-      powerline-fonts
-      dejavu_fonts
-      fira-code
-      fira-code-symbols
-      iosevka
-      iosevka-comfy.comfy
-      emacs-all-the-icons-fonts
-      nerdfonts
-      joypixels
-      etBook
-      vegur
-    ];
+    packages =
+      with pkgs;
+      [
+        powerline-fonts
+        dejavu_fonts
+        fira-code
+        fira-code-symbols
+        iosevka
+        iosevka-comfy.comfy
+        emacs-all-the-icons-fonts
+        joypixels
+        etBook
+        vegur
+      ]
+      ++ (builtins.filter lib.attrsets.isDerivation (builtins.attrValues nerd-fonts));
     fontconfig.defaultFonts = {
       monospace = [ "DejaVu Sans Mono for Powerline" ];
       sansSerif = [ "DejaVu Sans" ];
