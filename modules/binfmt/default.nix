8efbd6f57702d140455dfbe8db45da12f28b1008
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-12-06 11:38:42 +0100

    Enable `binfmt` also for `armv6l-linux`

--- modules/binfmt/default.nix
+++ modules/binfmt/default.nix
@@ -3,9 +3,10 @@
     "i686-linux"
     "aarch64-linux"
     "riscv64-linux"
+    "armv6l-linux"
   ];
   nix.extraOptions = ''
-    extra-platforms = aarch64-linux arm-linux i686-linux riscv64-linux
+    extra-platforms = aarch64-linux arm-linux i686-linux riscv64-linux armv6l-linux
   '';
 
   # XXX For some reason `docker buildx` isn't aware of this:
