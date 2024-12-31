3c92306ca31a0ebbdb3733342daa8ae284683d48
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-12-31 10:23:54 +0100

    Downgrade to Gerbera from Nixpkgs Stable (unstable is broken for aarch64-linux)

--- modules/mediatomb/default.nix
+++ modules/mediatomb/default.nix
@@ -1,6 +1,11 @@
+{ pkgs, ... }:
 {
   services.mediatomb = {
     enable = true;
+    # FIXME remove when fixed
+    package =
+      (builtins.getFlake "github:NixOs/nixpkgs/3ffbbdbac0566a0977da3d2657b89cbcfe9a173b")
+      .legacyPackages.${pkgs.stdenv.system}.gerbera;
     openFirewall = true;
     serverName = "Sisko";
     mediaDirectories = [
