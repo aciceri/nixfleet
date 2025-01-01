9590b879ed28c4450d8d432d87cba762ddf736fe
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-01-01 17:02:29 +0100

    Add `firefly-app-key` agenix secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -37,62 +37,52 @@ with keys.users;
   ];
   "autistici-password.age".publicKeys = [
     ccr-ssh
-
     kirk
     picard
     sisko
   ];
   "hercules-ci-join-token.age".publicKeys = [
     ccr-ssh
-
     mothership
     sisko
     picard
   ];
   "hercules-ci-binary-caches.age".publicKeys = [
     ccr-ssh
-
     mothership
     sisko
     picard
   ];
   "hercules-ci-secrets-json.age".publicKeys = [
     ccr-ssh
-
     mothership
     sisko
     picard
   ];
   "minio-credentials.age".publicKeys = [
     ccr-ssh
-
     picard
     sisko
   ];
   "aws-credentials.age".publicKeys = [
     ccr-ssh
-
     picard
     sisko
   ];
   "nextcloud-admin-pass.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "home-planimetry.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "home-assistant-token.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "chatgpt-token.age".publicKeys = [
     ccr-ssh
-
     kirk
     mothership
     picard
@@ -100,86 +90,74 @@ with keys.users;
   ];
   "cloudflare-dyndns-api-token.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "restic-hetzner-password.age".publicKeys = [
     ccr-ssh
-
     picard
     sisko
     kirk
   ];
   "hass-ssh-key.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "grafana-password.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "matrix-registration-shared-secret.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "matrix-sliding-sync-secret.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "forgejo-runners-token.age".publicKeys = [
     ccr-ssh
-
     picard
   ];
   "forgejo-nix-access-tokens.age".publicKeys = [
     ccr-ssh
-
     picard
   ];
   "garmin-collector-environment.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "hetzner-storage-box-sisko-ssh-password.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "sisko-restic-password.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "sisko-attic-environment-file.age".publicKeys = [
     ccr-ssh
-
+    sisko
+  ];
+  "firefly-app-key.age".publicKeys = [
+    ccr-ssh
     sisko
   ];
 
   # WireGuard
   "picard-wireguard-private-key.age".publicKeys = [
     ccr-ssh
-
     picard
   ];
   "sisko-wireguard-private-key.age".publicKeys = [
     ccr-ssh
-
     sisko
   ];
   "kirk-wireguard-private-key.age".publicKeys = [
     ccr-ssh
-
     kirk
   ];
   "deltaflyer-wireguard-private-key.age".publicKeys = [
     ccr-ssh
-
     deltaflyer
   ];
   "tpol-wireguard-private-key.age".publicKeys = [
