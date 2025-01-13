a98b467d26047ef56f07066c203cd8b0e2557591
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-01-13 16:43:47 +0100

    Fix swayidle
    My workaround is not needed anymore and it was preventing it to work

--- hmModules/swayidle/default.nix
+++ hmModules/swayidle/default.nix
@@ -48,10 +48,4 @@
         }
       ];
     };
-
-  # Otherwise it will start only after Sway and will not work with Hyprland
-  systemd.user.services.swayidle = {
-    Unit.PartOf = lib.mkForce [ ];
-    Install.WantedBy = lib.mkForce [ "graphical-session-pre.target" ];
-  };
 }
