e1ebdc68e99aeeb85203624b427b4de0f4d49255
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-02-27 14:06:04 +0700

    Improve Discord

--- hmModules/discord/default.nix
+++ hmModules/discord/default.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.discord ];
+  home.packages = [ (pkgs.discord.override {
+    withOpenASAR = true;
+    withVencord = true;
+  }) ];
   home.file.".config/discord/settings.json".text = builtins.toJSON {
     SKIP_HOST_UPDATE = true;
   };
