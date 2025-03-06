ff47e7ba4bea433090087ae2ed01801136b454a8
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-03-06 13:05:59 +0100

    Refactor

--- modules/pipewire/default.nix
+++ modules/pipewire/default.nix
@@ -1,6 +1,11 @@
 { lib, ... }:
 {
-  services.pipewire.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
 
   services.pulseaudio = {
     enable = false;
