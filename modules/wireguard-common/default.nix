9d4b2b4b7f80829f4a0f9ed0e925bf3844dc9fd4
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-03-14 17:18:47 +0100

    Remove useless generated /etc/hosts

--- modules/wireguard-common/default.nix
+++ modules/wireguard-common/default.nix
@@ -24,9 +24,4 @@
     privateKeyFile = config.age.secrets."${config.networking.hostName}-wireguard-private-key".path;
     listenPort = 51820;
   };
-
-  networking.hosts = lib.mapAttrs' (hostname: vpnConfig: {
-    name = vpnConfig.ip;
-    value = [ "${hostname}.wg.aciceri.dev" ];
-  }) vpn;
 }
