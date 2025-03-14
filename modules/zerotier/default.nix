bce8fbbb78a6f16e4e13b69a5717345e2fc228a9
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-03-14 18:09:29 +0100

    Correctly use `mkMerge`

--- modules/zerotier/default.nix
+++ modules/zerotier/default.nix
@@ -1,14 +1,17 @@
 { config, lib, ... }:
-lib.mkMerge [
-  {
-    services.zerotierone = {
-      enable = true;
-      joinNetworks = [ "632ea29085af0cb4" ];
-    };
-  }
-  (lib.optionalAttrs (config.networking.hostName == "sisko") {
-    environment.persistence."/persist".directories = [
-      "/var/lib/zerotier-one"
-    ];
-  })
-]
+{
+  config = lib.mkMerge [
+    {
+      services.zerotierone = {
+        enable = true;
+        joinNetworks = [ "632ea29085af0cb4" ];
+      };
+
+    }
+    (lib.mkIf (config.networking.hostName == "sisko") {
+      environment.persistence."/persist".directories = [
+        "/var/lib/zerotier-one"
+      ];
+    })
+  ];
+}
