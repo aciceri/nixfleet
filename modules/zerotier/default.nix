83e191134a570a07f00e99e05dbdcf3d047183dd
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-03-14 17:18:34 +0100

    Persist zerotierone state

--- modules/zerotier/default.nix
+++ modules/zerotier/default.nix
@@ -1,6 +1,14 @@
-{
-  services.zerotierone = {
-    enable = true;
-    joinNetworks = [ "632ea29085af0cb4" ];
-  };
-}
+{ config, lib, ... }:
+lib.mkMerge [
+  {
+    services.zerotierone = {
+      enable = true;
+      joinNetworks = [ "632ea29085af0cb4" ];
+    };
+  }
+  (lib.optionalAttrs (config.networking.hostName == "sisko") {
+    environment.persistence."/persist".directories = [
+      "/var/lib/zerotier-one"
+    ];
+  })
+]
