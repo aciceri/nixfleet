60674990832776aff8f20308e18a19678f0ef4ff
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-03-14 18:48:26 +0100

    Make zerotier work on all hosts

--- modules/zerotier/default.nix
+++ modules/zerotier/default.nix
@@ -1,17 +1,16 @@
 { config, lib, ... }:
 {
-  config = lib.mkMerge [
-    {
-      services.zerotierone = {
-        enable = true;
-        joinNetworks = [ "632ea29085af0cb4" ];
-      };
-
-    }
-    (lib.mkIf (config.networking.hostName == "sisko") {
-      environment.persistence."/persist".directories = [
-        "/var/lib/zerotier-one"
-      ];
-    })
-  ];
+  services.zerotierone = {
+    enable = true;
+    joinNetworks = [ "632ea29085af0cb4" ];
+  };
+  environment =
+    if (config.networking.hostName == "sisko") then
+      {
+        persistence."/persist".directories = [
+          "/var/lib/zerotier-one"
+        ];
+      }
+    else
+      { };
 }
