c65c53cf7398d9a85ee89d981abb8c15ba604cc0
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-03-27 15:34:51 +0100

    Fix screen sharing

--- hmModules/xdg/default.nix
+++ hmModules/xdg/default.nix
@@ -28,6 +28,29 @@
         ];
       };
     };
+    portal = {
+      enable = true;
+      configPackages = with pkgs; [
+        # xdg-desktop-portal-wlr
+        # xdg-desktop-portal-gtk
+        # xdg-desktop-portal-hyprland
+        xdg-desktop-portal-gnome
+        # xdg-desktop-portal
+        # kdePackages.xdg-desktop-portal-kde
+        # libsForQt5.xdg-desktop-portal-kde
+        gnome-keyring
+      ];
+      extraPortals = with pkgs; [
+        # xdg-desktop-portal-wlr
+        # xdg-desktop-portal-gtk
+        # xdg-desktop-portal-hyprland
+        xdg-desktop-portal-gnome
+        # xdg-desktop-portal
+        # kdePackages.xdg-desktop-portal-kde
+        gnome-keyring
+      ];
+    };
+
   };
   home.packages = [ pkgs.xdg-utils ];
 }
--- modules/xdg/default.nix
+++ modules/xdg/default.nix
@@ -1,29 +1,51 @@
 { pkgs, ... }:
 {
   xdg = {
+    autostart.enable = true;
+    menus.enable = true;
+    mime.enable = true;
+    icons.enable = true;
     portal = {
       enable = true;
       configPackages = with pkgs; [
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-hyprland
+        # xdg-desktop-portal-wlr
+        # xdg-desktop-portal-gtk
+        # xdg-desktop-portal-hyprland
         xdg-desktop-portal-gnome
-        xdg-desktop-portal
-        kdePackages.xdg-desktop-portal-kde
-        libsForQt5.xdg-desktop-portal-kde
+        # xdg-desktop-portal
+        # kdePackages.xdg-desktop-portal-kde
+        # libsForQt5.xdg-desktop-portal-kde
         gnome-keyring
       ];
       extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-hyprland
+        # xdg-desktop-portal-wlr
+        # xdg-desktop-portal-gtk
+        # xdg-desktop-portal-hyprland
         xdg-desktop-portal-gnome
-        xdg-desktop-portal
-        kdePackages.xdg-desktop-portal-kde
+        # xdg-desktop-portal
+        # kdePackages.xdg-desktop-portal-kde
         gnome-keyring
       ];
       xdgOpenUsePortal = true;
       wlr.enable = true;
+    };
+
+  };
+  security.rtkit.enable = true;
+
+  security.polkit.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  systemd.user.services.niri-flake-polkit = {
+    description = "PolicyKit Authentication Agent provided by niri-flake";
+    wantedBy = [ "niri.service" ];
+    after = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.libsForQt5.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
     };
   };
 }
