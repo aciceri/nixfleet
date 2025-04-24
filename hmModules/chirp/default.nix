403029e478ed92bfbfa16a421a38471a79bdb589
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-04-24 14:05:20 +0200

    Unused comments

--- hmModules/chirp/default.nix
+++ hmModules/chirp/default.nix
@@ -1,8 +1,4 @@
 { pkgs, ... }:
-let
-in
-# nixpkgs = builtins.getFlake "github:NixOS/nixpkgs/932fc16b263f26803d3960e4400bc13dde84a972";
-# chirp = nixpkgs.legacyPackages.${pkgs.system}.chirp;
 {
   home.packages = [ pkgs.chirp ];
 }
