79fd28061b4f19995b91f10ea4360ee1a4a69a76
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-04-24 14:05:29 +0200

    Use `sdrangel` from an old commit (it's broken)

--- hmModules/sdrangel/default.nix
+++ hmModules/sdrangel/default.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
   home.packages = [
-    pkgs.sdrangel
+    (builtins.getFlake "github:NixOS/nixpkgs/063dece00c5a77e4a0ea24e5e5a5bd75232806f8")
+    .legacyPackages.${pkgs.system}.sdrangel
     pkgs.kdePackages.qtlocation
   ];
 }
