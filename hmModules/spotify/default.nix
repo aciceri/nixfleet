995af1451562f006b2045af4f419433707d99cb0
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-05-16 16:02:32 +0200

    Disable spotify at startup

--- hmModules/spotify/default.nix
+++ hmModules/spotify/default.nix
@@ -9,18 +9,18 @@ in
 {
   home.packages = [ spotify-adblocked ];
 
-  systemd.user.services.spotify-adblocked = {
-    Install.WantedBy = [ "graphical-session.target" ];
+  # systemd.user.services.spotify-adblocked = {
+  #   Install.WantedBy = [ "graphical-session.target" ];
 
-    Unit = {
-      Description = "Spotify";
-      PartOf = [ "graphical-session.target" ];
-    };
+  #   Unit = {
+  #     Description = "Spotify";
+  #     PartOf = [ "graphical-session.target" ];
+  #   };
 
-    Service = {
-      ExecStart = lib.getExe spotify-adblocked;
-      Restart = "on-failure";
-      RestartSec = 3;
-    };
-  };
+  #   Service = {
+  #     ExecStart = lib.getExe spotify-adblocked;
+  #     Restart = "on-failure";
+  #     RestartSec = 3;
+  #   };
+  # };
 }
