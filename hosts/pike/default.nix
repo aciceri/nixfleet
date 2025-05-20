839cf0df5fe01222c8502de6d2a464465e9c3e79
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-05-20 09:40:48 +0200

    Use NVidia GPU

--- hosts/pike/default.nix
+++ hosts/pike/default.nix
@@ -169,4 +169,16 @@
     enable = true;
     algorithm = "zstd";
   };
+
+  hardware.nvidia-container-toolkit.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
 }
