959016455f865cef60f6c080a2b5c082a634ac76
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-05-23 21:32:48 +0200

    Remove unused caches

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -22,18 +22,18 @@
       ];
       netrc-file = config.age.secrets.nix-netrc.path;
       substituters = [
-        #   "s3://cache?profile=default&region=eu-south-1&scheme=https&endpoint=cache.aciceri.dev"
         "https://cache.iog.io"
         "https://cache.lix.systems"
         "https://nix-community.cachix.org"
-        "https://mlabs.cachix.org"
+        # "https://mlabs.cachix.org"
+        "http://sisko.wg.aciceri.dev:8081/nixfleet"
       ];
       trusted-public-keys = [
-        #   "cache.aciceri.dev~1:nJMfcBnYieY2WMbYDG0s9S5qUhU+V4RPL+X9zcxXxZY="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "mlabs.cachix.org-1:gStKdEqNKcrlSQw5iMW6wFCj3+b+1ASpBVY2SYuNV2M="
+        # "mlabs.cachix.org-1:gStKdEqNKcrlSQw5iMW6wFCj3+b+1ASpBVY2SYuNV2M="
+        "nixfleet:Bud23440n6mMTmgq/7U+mk91zlLjnx2X3lQQrCBCCU4="
       ];
       deprecated-features = [ "url-literals" ];
     };
