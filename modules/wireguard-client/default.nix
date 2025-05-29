4e90abc309e78b334d1fbba59a416119a293c33b
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-05-29 15:34:51 +0200

    Allow full tunnel

--- modules/wireguard-client/default.nix
+++ modules/wireguard-client/default.nix
@@ -13,6 +13,7 @@
       {
         publicKey = vpn.sisko.publicKey;
         allowedIPs = [ "10.100.0.0/24" ];
+        # allowedIPs = [ "0.0.0.0/24" ]; # Uncomment for full tunnel
         endpoint = "vpn.aciceri.dev:51820";
         persistentKeepalive = 25;
       }
