a5619aa74dad6801abb3e7931153bc3bed9829f7
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-07-27 17:47:05 +0200

    Gemini flash 2.5 model is no more a preview

--- hmModules/emacs/init.el
+++ hmModules/emacs/init.el
@@ -1043,13 +1043,13 @@ This is meant to be an helper to be called from the window manager."
 
 (use-package gptel
   :custom
-  (gptel-model 'google/gemini-2.5-flash-preview)
+  (gptel-model 'google/gemini-2.5-flash)
   (gptel-backend (gptel-make-openai "OpenRouter"
 		           :host "openrouter.ai"
 		           :endpoint "/api/v1/chat/completions"
 		           :key (lambda () (require 'f) (f-read-text (getenv "OPENROUTER_API_KEY_PATH")))
 		           :stream t
-		           :models '(google/gemini-2.5-flash-preview))
+		           :models '(google/gemini-2.5-flash))
 		         )
   (gptel-default-mode 'org-mode)
   (gptel-org-branching-context nil) ;; this is cool but I don't feel comfortable with it
